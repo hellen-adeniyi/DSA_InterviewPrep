bc95a1f3d23422a6f07c32b2495b3acad60e44a1
hellen-adeniyi <hellenadeniyi29@gmail.com>
2025-10-06 09:56:42 -0600

    completed the hey programmer function

--- Introduction/heyProgrammer.java
+++ Introduction/heyProgrammer.java
@@ -1,5 +1,12 @@
 package Introduction;
 
 public class heyProgrammer {
+
+    public static String greet(String s){
+        return "hey "+ s;
+    }
+    public static void main(String[] args) {
+        
+    }
     
 }
