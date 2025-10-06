77c1940c9894bf5ffd3fd5d6380daecdd6da6ee2
hellen-adeniyi <hellenadeniyi29@gmail.com>
2025-10-06 10:04:51 -0600

    added regression tests

--- Introduction/heyProgrammer.java
+++ Introduction/heyProgrammer.java
@@ -1,11 +1,28 @@
 package Introduction;
 
+
+
+/**
+ * This function greet takes a string argumenr,s, and returns the string "hey s"
+ * where s is the string passed to the function.
+ */
 public class heyProgrammer {
 
     public static String greet(String s){
         return "hey "+ s;
     }
     public static void main(String[] args) {
+
+        // Regression tests
+        String al = heyProgrammer.greet("alvin");
+        String ja= heyProgrammer.greet("jason");
+
+        if(al != "hey alvin")
+            {System.out.println("Test Case Failed");}
+
+        
+        if(ja != "hey jason")
+            {System.out.println("Test Case Failed");}
         
     }
     
